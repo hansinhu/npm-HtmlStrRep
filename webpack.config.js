4de3a40873bc6f361d446747269ee9b4295de411
hansin <huxinghan@loonxi.com>
2018-09-14 11:20:15 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
-  entry: './index.js',
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
