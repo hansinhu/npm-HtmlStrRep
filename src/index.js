3f635102f6c3bd000fc28cd805efa0c72eb9f066
hansin <huxinghan@loonxi.com>
2018-09-14 10:44:20 +0800

    build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 
 // str: 目标字符串，sFrom: 被替换的字符串， sTo:  替换成字符串,
+// import HtmlStrRep from '@/common/htmlStrRep.js'
+// const strRep = new HtmlStrRep()
+// strRep.startRep(n.data.enquiryContent, params.keyword, '#f44c3f')
 
-export default class HtmlStrRep {
+export default class htmlStrRep {
   constructor () {
     this.endObj = {
       tag: '',
@@ -10,29 +13,26 @@ export default class HtmlStrRep {
     }
     this.originStr = ''
     this.sFrom = ''
-    this.sTo = ''
+    this.sToColor = ''
   }
 
-  startRep (str, sFrom, sTo = '') {
+  startRep (str, sFrom, color = '#f44c3f') {
     this.originStr = str
     this.sFrom = sFrom
-    this.sTo = sTo
-    if (str.indexOf(sFrom) === -1 || !sFrom) {
-      return str
-    }
+    this.sToColor = color
     this.endObj = this.strToObj(str)
-    let changeStr = this.reGroup(this.endObj, sTo)
+    let changeStr = this.reGroup(this.endObj)
     return changeStr
   }
 
   // 把树形对象重新组合，没有tag的执行替换
-  reGroup (splitObj, sTo) {
+  reGroup (splitObj) {
     let groupStr = ''
     if (splitObj.tag) {
       let strArr = []
       // 递归组合
       splitObj.children.forEach((n) => {
-        strArr.push(this.reGroup(n, sTo))
+        strArr.push(this.reGroup(n))
       })
       groupStr += strArr.join(splitObj.tag)
     } else {
@@ -43,7 +43,13 @@ export default class HtmlStrRep {
   }
 
   replace (str) {
-    return str.split(this.sFrom).join(this.sTo)
+    return str.replace(new RegExp(this.sFrom, 'gi'), (key) => {
+      if (key !== '') {
+        return '<span style="color: ' + this.sToColor + ';">' + key + '</span>'
+      } else {
+        return ''
+      }
+    })
   }
 
   // 把字符串递归切割成树形对象
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,6 @@ module.exports = {
   },
   plugins: [
     // tree shaking
-    // new UglifyJSPlugin()
+    new UglifyJSPlugin()
   ]
 }
